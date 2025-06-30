a9b7bd7191b15f687a3dc452d6d2bee44f87fb5b
RayaneGOUDA <rayane.gouda@gmail.com>
2025-06-30 22:58:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ def generate_url():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+if __name__ == '__main__':
+    app.run(debug=True)
