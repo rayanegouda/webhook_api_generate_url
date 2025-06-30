0056198dfb6fb9ae3a6e52e4e414412d4f6b8b8d
RayaneGOUDA <rayane.gouda@gmail.com>
2025-07-01 00:09:25 +0200

    Update application.py

--- application.py
+++ application.py
@@ -56,5 +56,4 @@ def generate_url():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
